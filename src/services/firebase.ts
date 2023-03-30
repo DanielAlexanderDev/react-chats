a7a5a8efe3a836a143761a3262d2240d7f7322de
Daniel Alexander <daniel.llumiquinga@hotmail.com>
2023-03-30 15:01:26 -0500

    [fix] auth domain

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -9,7 +9,7 @@ import { getFirestore } from "firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "https://react-chats-two.vercel.app/",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
