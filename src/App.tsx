0c06c7c4ac748a0e518058f8a5a121733266adb5
Daniel Alexander <daniel.llumiquinga@hotmail.com>
2023-03-30 14:42:12 -0500

    [Add] Auth routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Chat from "./pages/Chat/Chat";
+import Home from "./pages/Home/Home";
+import { auth } from "./services/firebase";
 
 function App() {
-  console.log();
-  return <div className="App"></div>;
+  const [user] = useAuthState(auth);
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/chat" element={user ? <Chat /> : <Navigate to={"/"} />} />
+    </Routes>
+  );
 }
 
 export default App;
